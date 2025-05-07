fda6b6118fd78100356f9b5f5eeb9ac3197b5c19
Ali-Ism <ali.samir.ismail03@gmail.com>
2025-05-07 12:08:23 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,18 @@ provider "aws" {
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   skip_credentials_validation = true
+
   endpoints {
     ec2 = "http://ip10-0-2-6-cvh9tgab9qb14bivkplg-4566.direct.lab-boris.fr"
   }
 }
 
+variable "commit_sha" {
+  description = "Git commit hash to generate unique AMI"
+  type        = string
+}
+
 resource "aws_instance" "demo" {
-  ami           = "ami-12345678"
+  ami           = "ami-${var.commit_sha}"
   instance_type = "t2.micro"
 }
